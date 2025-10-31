dd3be2c053fb0eff04cc5c1a16ea5859e46bd5c0
pt131923 <165816510+pt131923@users.noreply.github.com>
2025-10-31 17:33:05 +0700

    update

--- src/authors/authors.controller.ts
+++ src/authors/authors.controller.ts
@@ -1,5 +1,5 @@
 import { Controller, Get, Post, Body, Patch, Param, Delete, UseGuards } from '@nestjs/common';
-import { ApiBearerAuth, ApiBody, ApiQuery, ApiTags } from '@nestjs/swagger';
+import { ApiBearerAuth, ApiBody, ApiHeader, ApiQuery, ApiTags } from '@nestjs/swagger';
 import { AuthorsService } from './authors.service';
 import { CreateAuthorDto } from './dtos/create_author.dto';
 import { UpdateAuthorDto } from './dtos/update_author.dto';
@@ -12,6 +12,12 @@ export class AuthorsController {
   constructor(private readonly authorsService: AuthorsService) {}
 
   @Post()
+  @ApiBearerAuth()
+  @UseGuards(AuthGuard('jwt'), RolesGuard)
+  @ApiHeader({ name: 'x-api-name', required: true, description: 'Tên tác giả' })
+  @ApiHeader({ name: 'x-api-bio', required: true, description: 'Mô tả tác giả' })
+  @ApiHeader({ name: 'x-api-date_of_birth', required: true, description: 'Ngày sinh tác giả' })
+  @ApiHeader({ name: 'x-api-email', required: true, description: 'Email tác giả' })
   @ApiBody({
     description: 'Mẫu tạo tác giả',
     type: CreateAuthorDto,
@@ -47,6 +53,10 @@ export class AuthorsController {
   @Patch(':id')
   @ApiBearerAuth()
   @UseGuards(AuthGuard('jwt'), RolesGuard)
+  @ApiHeader({ name: 'x-api-name', required: true, description: 'Tên tác giả' })
+  @ApiHeader({ name: 'x-api-bio', required: true, description: 'Mô tả tác giả' })
+  @ApiHeader({ name: 'x-api-date_of_birth', required: true, description: 'Ngày sinh tác giả' })
+  @ApiHeader({ name: 'x-api-email', required: true, description: 'Email tác giả' })
   @ApiBody({
     description: 'Mẫu cập nhật tác giả',
     type: UpdateAuthorDto,
